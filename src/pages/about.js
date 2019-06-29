873a990c24d7289704407b5789cb188985b74c31
Nammyoung <hongnammyoung@gmail.com>
2019-06-29 18:05:05 +0200

    add layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default () => (
-  <div>
-    <h1> 자기소개 </h1>
-    <p> 도시와 예술을 공부합니다 </p>
-    <Link><p> 뒤로 </p></Link>
-  </div>
+  <Layout>
+    <div>
+      <h1> 자기소개 </h1>
+      <p> 도시와 예술을 공부합니다 </p>
+      <Link>
+        <p> 뒤로 </p>
+      </Link>
+    </div>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
+import Layout from "../components/layout"
 import { Link } from "gatsby"
 
 export default () => (
-  <div>
+  <Layout>
     <h1>도시의 시공간을 공부합니다</h1>
-    <p></p>
-    <Link to="/about/">
-      {" "}
-      <h2>Hong Nammyoung</h2>{" "}
-    </Link>
-    <p>nam is starting!</p>
-    <img src="images/aachener_dom.jpg" alt="" height="400" />
-    <Link to="/test/"> 자기소개</Link>
-  </div>
+    <div>
+      <p></p>
+      <Link to="/about/">
+        {" "}
+        <h2>Hong Nammyoung</h2>{" "}
+      </Link>
+      <p>nam is starting!</p>
+      <img src="images/aachener_dom.jpg" alt="" height="400" />
+      <Link to="/test/"> 자기소개</Link>
+    </div>
+  </Layout>
 )
