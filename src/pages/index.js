8be31fb3d26631fd22cb80b54cdf794d7beb99e1
Nammyoung <hongnammyoung@gmail.com>
2019-06-29 17:35:38 +0200

    add typo module

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <h1>dkanjsirh</h1>
+    <h1>도시의 시공간을 공부합니다</h1>
+    <p></p>
     <Link to="/about/">
       {" "}
       <h2>Hong Nammyoung</h2>{" "}
     </Link>
     <p>nam is starting!</p>
     <img src="images/aachener_dom.jpg" alt="" height="400" />
-    <Link to="/test/"> 정신좀 차려라!</Link>
+    <Link to="/test/"> 자기소개</Link>
   </div>
 )
--- src/pages/test.js
+++ src/pages/test.js
@@ -2,8 +2,9 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1> 테스트페이지입니다 </h1>
-    <p> 도시와 예술을 공부합니다 </p>
+    <h1> 자기소개 </h1>
+    <h2>홍남명</h2>
+    <p> 도시의 시공간에 애정을 쏟고 있습니다 </p>
     
   </div>
 )
